a875ebcbe4cb29988cdd1d757eb441cd99f8ba66
Tyler Gajewski <gajewskies@gmail.com>
2015-07-02 01:13:53 -0400

    fixing

--- mods/apiResponse.js
+++
@@ -1,8 +0,0 @@
-// Data transfer class to move dave from the controller
-
-var apiResponse = function (configure) {
-	this.success = configure.success;
-	this.extras = configure.extras;
-};
-
-module.exports = apiResponse;
--- mods/userProfile.js
+++
@@ -1,8 +0,0 @@
-// seperate file to keep sensitive information from being transfered
-var userProfile = function(confiure) {
-	this.email = configure.email,
-	this.firstname = configure.firstname,
-	this.lastname = configure.lastname
-};
-
-module.exports = userProfile;
